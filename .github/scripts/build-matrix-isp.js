eb30ee41b395c0725097c898773ce33b6c6a4f0b
Florent REUTER <freuter.externe@bedrockstreaming.com>
2024-08-02 11:25:14 +0200

    wip

--- .github/scripts/build-matrix-isp.js
+++ .github/scripts/build-matrix-isp.js
@@ -1,22 +1,25 @@
 
-console.log('Starting build matrix function');
-
 module.exports = ({ context, core }) => {
-    console.log('Building matrix');
-    console.log('Environment variables:', process.env);
-    const matrix = process.env.PATHS.terraform.map(path => {
-        console.log(`Mapping path: ${path}`);
-        const isp = path.startsWith(".cloud/terraform/blabla") ? process.env.ISP.find(i => i.name === 'blabla') : process.env.ISP.find(i => i.name === 'blibli');
-        console.log(`ISP: ${isp}`);
-        return {
-            path: path,
-            workspace: isp.workspace,
-            'working-directory': isp['working-directory'],
-            role: isp.role,
-            runner: isp.runner
-        };
-    });
+  console.log('Building matrix');
+  console.log('Environment variables:', process.env);
+  const paths = process.env.PATHS.split(',');
+  const ispData = JSON.parse(process.env.ISP);
 
-    console.log('Matrix:', matrix);
-    return matrix;
+  console.log('Paths:', paths);
+  console.log('ISP data:', ispData);
+
+  const pathNameMapping = paths.map(path => {
+    console.log(`Mapping path: ${path}`);
+    const matchingIsp = ispData.find(isp => isp['working-directory'] === path);
+    console.log(`Matching ISP:`, matchingIsp);
+
+    return {
+      path,
+      name: matchingIsp ? matchingIsp.name : 'blabla'
+    };
+  });
+
+  console.log('Path name mapping:', pathNameMapping);
+
+  return pathNameMapping;
 };
